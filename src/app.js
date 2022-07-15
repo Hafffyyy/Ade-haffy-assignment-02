963426f40b4eb3fa3fa947374dc5d227932befa9
haffy1420 <106037412+Hafffyyy@users.noreply.github.com>
2022-07-15 21:37:05 +0000

    Attempted question number 1 and 2.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,13 @@ const { sumOfNumbers, countEvenNumbers, celsiusToFahrenheit } = assignment;
 
 if (sumOfNumbers && typeof sumOfNumbers === 'function'){
     console.log(sumOfNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const even = arr.filter(number => {
+  return number % 2 === 0;
+});
+
+console.log(even); // 👉️ [2, 4, 6, 8]
 }
 
 if (countEvenNumbers && typeof countEvenNumbers === 'function'){
